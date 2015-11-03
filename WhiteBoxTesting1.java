a1bd527b6b0fcad0c705b585f596bbc6701dc0a6
Kira Gobes <kira.gobes@gmail.com>
2015-11-02 21:53:29 -0500

    WhiteBox for RentalProduct

---
+++ WhiteBoxTesting1.java
@@ -0,0 +1,13 @@
+public class WhiteBoxTesting1{
+  public static void main(String[] args){
+    //Testing for Rental Product
+    System.out.println("Creating new rental product:");
+    RentalProduct rp = new RentalProduct(123, 20, "Red Canoe", 5);
+    System.out.println("Description: " + rp.getDescription() + " Should display Red Canoe");
+    System.out.println("Price: $" + rp.getPrice() + " Should display $20");
+    System.out.println("Duration: " + rp.getDuration() + " days Should display 5 days");
+    rp.setDuration(7); //change duration 
+    System.out.println("New duration: " + rp.getDuration() + " days Should display 7 days");
+    System.out.println("Valid product? " + rp.validate() +" Should display false");
+  }
+}
