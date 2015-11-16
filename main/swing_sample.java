e41761360f421ebd42108e1db41860b13eb22696
Kira Gobes <kira.gobes@gmail.com>
2015-11-15 22:08:27 -0500

    UI Round 3
    Runtime error, not sure why, someone help
    Started to implement the process sale, when I added functionality to
    the “enter” button for putting in an item id, it broke

--- main/swing_sample.java
+++ main/swing_sample.java
@@ -1,8 +1,12 @@
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.Scanner;
+import java.sql.*;
 public class swing_sample extends JFrame
 {
+  public static Register register = new Register();
+  public static Scanner keyboard = new Scanner(System.in);
     //declaring our swing components
     JLabel l_name,l_pass;
     JTextField t_name;
@@ -11,6 +15,15 @@ public class swing_sample extends JFrame
     Container c;
     JFrame frame2;
     JPanel panel2;
+    
+    //Sale Stuff:
+    JFrame processSaleFrame;
+    JPanel processSale;
+    JLabel title, enterId, enterQuan;
+    JTextField id, quan;
+    JButton enter;
+    JLabel item;
+    JButton sale;
  
     //a inner class to handling ActionEvents
     handler handle;
@@ -83,39 +96,108 @@ public class swing_sample extends JFrame
                     //frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                     frame2.setLocationByPlatform(true);
 
-                    JButton sale = new JButton("Sale");
+                    sale = new JButton("Sale");
                     JButton rental = new JButton("Rental");
                     JButton returnItems = new JButton("Return");
                     frame2.add(panel2);
                     panel2.add(sale);
+                    panel2.add(rental);
+                    panel2.add(returnItems);
                     frame2.setSize(200,300);
                     
                     
                     
+            
         
                     sale.addActionListener(new ActionListener()
                     {
                        public void actionPerformed(ActionEvent ae)
                           {
                          //if clicked then sale things should happen
-                          System.out.println("HAI");
-            }
-        });
-                    frame2.setVisible(true);
-                    c.setVisible(false);
-        
-
-        // Adding the button to the South side of the frame1.
-        //frame1.add(LoginButton, BorderLayout.PAGE_END);
-        //frame1.pack();
-        //frame1.setVisible(true);
-                }
-                else
+                          processSaleFrame= new JFrame("Process Sale");
+                          processSale= new JPanel();
+                          title = new JLabel("Process Sale");
+                          enterId = new JLabel("Enter an ID of an item ");
+                          enterQuan = new JLabel("Quantity: ");
+                          id =new JTextField(10);
+                          quan = new JTextField(10);
+                          enter = new JButton("Enter");
+                          
+                          processSaleFrame.add(processSale);
+                          processSale.add(title);
+                          processSale.add(enterId);
+                          processSale.add(id);
+                          processSale.add(enterQuan);
+                          processSale.add(quan);
+                          processSale.add(enter);
+                          
+                          processSaleFrame.setSize(200,300);
+                          processSaleFrame.setVisible(true);
+                          frame2.setVisible(true);
+                          c.setVisible(false);
+                          
+                          
+                       }
+                        });
+                 
+                    //when an item gets entered:
+                      enter.addActionListener(new ActionListener()
+                    {
+                       public void actionPerformed(ActionEvent ae)
+                          {
+                         item = new JLabel(id.getText() + "    " + quan.getText());
+                         processSale.add(item);
+                         
+                          /*boolean orderComplete = false;
+                          int productID = 0, quantity = 0;
+                          double amountPaid = 0.0;
+                          
+                          register.makeOrder('S');
+                          while(!orderComplete){
+                            item = new JLabel(id.getText() + "    " + quan.getText());
+                            
+                            System.out.println(register.order());
+                            System.out.println("Enter an ID of an item to add or -1 to finish adding items: ");
+                            productID = keyboard.nextInt();
+                            if(productID >= 0){
+                              System.out.println("Enter a quantity: ");
+                              quantity = keyboard.nextInt();
+                              register.order().addLineItem(register.getProduct(productID), quantity, 0);
+                            }
+                            else
+                              orderComplete = true;
+                          }
+                          System.out.println(register.order());
+                          System.out.println("Displaying Total: $" + register.order().getSubtotal() * register.TAXRATE);
+                          System.out.println("Enter amount to pay: ");
+                          amountPaid = keyboard.nextDouble();
+                          System.out.println("Your change is $" + (amountPaid - register.order().getSubtotal() * register.TAXRATE));
+                          //Store transaction in the database
+                          System.out.println("Transaction complete. Returning to main menu.");
+                         
+                         */
+                       }
+            }); 
+                    
+                    
+                    
+                    
+                    
+                }         
+                    
+                    
+           else
                 {
                     //a pop-up box
                     JOptionPane.showMessageDialog(null, "Login failed!","Failed!!",
                                         JOptionPane.ERROR_MESSAGE);
                 }
+
+        // Adding the button to the South side of the frame1.
+        //frame1.add(LoginButton, BorderLayout.PAGE_END);
+        //frame1.pack();
+        //frame1.setVisible(true);
+                
             }
         }
  
